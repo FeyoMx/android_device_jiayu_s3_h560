ca97d8b9fe49b23e2ca5f1e15d2df4e134e7e874
FeyoMx <elfeyo1980@gmail.com>
2016-09-14 09:18:50 -0500

    Update device_s3_h560.mk
    adapting to aio_otfp

--- device_s3_h560.mk
+++ device_s3_h560.mk
@@ -4,18 +4,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from hardware-specific part of the product configuration
-$(call inherit-product, device/jiayu/s3_h560/device.mk)
+$(call inherit-product, device/lenovo/aio_otfp/device.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := s3_h560
+PRODUCT_RELEASE_NAME := aio_otfp
 
 EXTENDED_FONT_FOOTPRINT := true
 
-PRODUCT_DEVICE := s3_h560
-PRODUCT_NAME := full_s3_h560
-PRODUCT_BRAND := Jiayu
-PRODUCT_MODEL := S3
-PRODUCT_MANUFACTURER := Jiayu
+PRODUCT_DEVICE := aio_otfp
+PRODUCT_NAME := full_aio_otfp
+PRODUCT_BRAND := Lenovo
+PRODUCT_MODEL := K3 Note
+PRODUCT_MANUFACTURER := Lenovo
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 # Boot animation
@@ -23,5 +23,5 @@ TARGET_SCREEN_HEIGHT      := 1920
 TARGET_SCREEN_WIDTH       := 1080
 TARGET_BOOTANIMATION_NAME := 1080
 
-PRODUCT_DEFAULT_LANGUAGE := en
+PRODUCT_DEFAULT_LANGUAGE := es
 PRODUCT_DEFAULT_REGION   := US
