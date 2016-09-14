3ccf340b0d17fd88a2c1c1efb065ca2c49b9541c
FeyoMx <elfeyo1980@gmail.com>
2016-09-14 09:32:21 -0500

    Update cm.mk
    adapting to aio_otfp

--- cm.mk
+++ cm.mk
@@ -1,11 +1,11 @@
-$(call inherit-product, device/jiayu/s3_h560/device_s3_h560.mk)
+$(call inherit-product, device/lenovo/aio_otfp/device_aio_otfp.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := s3_h560
-PRODUCT_NAME := cm_s3_h560
-PRODUCT_BRAND := Jiayu
-PRODUCT_MODEL := S3
-PRODUCT_MANUFACTURER := Jiayu
+PRODUCT_DEVICE := aio_otfp
+PRODUCT_NAME := cm_aio_otfp
+PRODUCT_BRAND := Lenovo
+PRODUCT_MODEL := K3 Note
+PRODUCT_MANUFACTURER := Lenovo
